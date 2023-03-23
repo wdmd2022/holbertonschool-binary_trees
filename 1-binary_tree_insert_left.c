5232efecd97a87647458373951e9ac7e2c142156
Douglas <wdmdavison@icloud.com>
2023-03-22 21:09:57 -0700

    fix NULL parent handling for inserts left and right

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (!parent)
+	{
+		return (NULL);
+	}
 	new = malloc(sizeof(binary_tree_t));
-	if ((!new) || (!parent))
+	if (!new)
 	{
 		return (NULL);
 	}
